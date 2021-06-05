4d16ce7bcba9d7f870c46b48fdb833668073eeca
Karol Stryczek <karol531531@gmail.com>
2021-06-05 13:33:08 +0200

    Refactoring

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 midi_songs_path = 'input/midi_songs/*.mid'
 midi_songs_serialized_path = 'input/midi_songs_serialized/songs_serialized'
 
+# Training parameters
 sequence_length = 80
 epochs_amount = 50
 batch_size = 512
+continue_training = True
 
+# Generation parameters
 batch_generation_size = 1
 output_length = 50
-
-continue_training = True
